1fc8186bac07730982b6bd3928ccd4103df02ecc
daniello7 <daniellopezolmos7@gmail.com>
2024-10-04 16:52:41 +0200

    Fin paso 3 (5 abris)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inicio</title>
-</head>
-<body class="font-sans antialiased dark:bg-black dark:text-white/50">
-<h1>Inicio</h1>
-<ul>
-    <li><a href="{{ route('home') }}">Home</a></li>
-    <li><a href="{{ route('blog') }}">Blog</a></li>
-    <li><a href="{{ route('about') }}">Nosotros</a></li>
-    <li><a href="{{ route('contact') }}">Contacto</a></li>
-</ul>
-</body>
-</html>
+<x-layout meta-title="Inicio" meta-description="Descripción de la página de Inicio">
+    <h1>Inicio</h1>
+    <x-slot:sidebar>
+        <p>Home Sidebar</p>
+    </x-slot:sidebar>
+</x-layout>
