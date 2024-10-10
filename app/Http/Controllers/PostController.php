318f47560b1d613ee94aa6cffc3f6768b800c28b
daniello7 <daniellopezolmos7@gmail.com>
2024-10-10 15:44:44 +0200

    Fin paso 10 (abris)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,11 +10,14 @@ class PostController extends Controller
     {
         $posts = Post::all();
 
-        return view('blog', compact('posts'));
+        return view('posts.index', compact('posts'));
     }
 
     public function show(Post $post)
     {
-
+        return view('posts.show', compact('post')); // Es lo mismo que ['post' => $post]
+        // Si recibe $id de parámetro
+        // $post = Post::find($id)
+        // return 'Detalle del post ' . $post->title;
     }
 }
