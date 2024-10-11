6888518cb05196a6f88acd6d9233e3234914764c
daniello7 <daniellopezolmos7@gmail.com>
2024-10-11 16:52:48 +0200

    Fin paso 15, lang github, actualizado y añadido vista formulario, traducido y añadido a json

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,9 +1,9 @@
 <x-layout meta-title="Create a new Post" meta-description="Form to create a new Post">
-    <h1>Create a new Post</h1>
+    <h1>{{ __('Create a new Post') }}</h1>
     <form method="POST" action="{{ route('posts.store') }}">
         @csrf
         <label>
-            Title <br>
+            {{ __('Title') }} <br>
             <input type="text" name="title" value="{{ old('title') }}">
             @error('title')
             <br>
@@ -12,7 +12,7 @@
         </label>
         <br>
         <label>
-            Body <br>
+            {{ __('Body') }} <br>
             <textarea name="body"></textarea>
             @error('body')
             <br>
@@ -20,8 +20,8 @@
             @enderror
         </label>
         <br>
-        <button type="submit">Send</button>
+        <button type="submit">{{ __('Send') }}</button>
         <br>
     </form>
-    <a href="{{ route('posts.index') }}">Back</a>
+    <a href="{{ route('posts.index') }}">{{ __('Back') }}</a>
 </x-layout>
