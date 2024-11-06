7fbb0351d5c73b752cce5b2e79378dd00cd6125b
daniello7 <daniellopezolmos7@gmail.com>
2024-11-06 16:25:03 +0100

    Ejercicio 6, Creado factories y seeder para user y posts

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -47,8 +48,8 @@ class User extends Authenticatable implements MustVerifyEmail
         ];
     }
 
-    public function posts(): BelongsToMany
+    public function posts(): hasMany
     {
-        return $this->belongsToMany(Post::class);
+        return $this->hasMany(Post::class);
     }
 }
